98e8dd40adb05eb5b7e5c2b8a503c49ea0ff4014
DannyMan7777 <unthinkableplayz28@gmail.com>
2026-09-09 14:13:32 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Daniel McKinney
+* Course: CSCI 1250, Section 002
+* Assignment Lab 0, Hellow World
+Date: September 9, 2026
+Description: Prints a greeting and a short introduction to the console.
+*/
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Daniel McKinney.");
+Console.WriteLine("This is my first program at ETSU.");
